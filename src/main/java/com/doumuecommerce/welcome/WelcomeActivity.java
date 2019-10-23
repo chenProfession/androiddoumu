4222011bcd724dbbaec4d273ca7bc5358325208a
cheng <ccelephant2012@gmail.com>
2019-10-24 03:08:05 +0800

    first commit

---
+++ src/main/java/com/doumuecommerce/welcome/WelcomeActivity.java
@@ -0,0 +1,50 @@
+package com.doumuecommerce.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.doumuecommerce.authorization.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //设置竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    private void init(){
+        textView = (TextView) findViewById(R.id.textVersion);
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            textView.setText("V" + packageInfo.versionName);
+        }catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            textView.setText("V");
+        }
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        };
+        timer.schedule(timerTask,3000);
+    }
+}
